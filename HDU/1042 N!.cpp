67c40c60d6adf352bb7a14932d4e351d0c8893c3
Little Von <122691411@qq.com>
2017-05-16 14:11:31 +0800

    数组模拟大数乘法
    用数组去模拟大数的乘法即可完成阶乘

---
+++ HDU/1042 N!.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int SIZE = 1e5;
+int num[SIZE];
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    while (~scanf("%d", &n))
+    {
+        memset(num, 0, sizeof(num));
+        num[0] = num[1] = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            int len = num[0];
+            for (int j = 1; j <= len; ++j)
+            {
+                num[j] *= i;
+            }
+            for (int j = 1; j <= num[0]; ++j)
+            {
+                if(num[j]>9)
+                {
+                    num[j+1] += num[j]/10;
+                    num[j] %= 10;
+                }
+                if(num[num[0]+1]) ++num[0];
+            }
+        }
+        for (int i = num[0]; i >= 1; --i)
+        {
+            putchar(num[i]+'0');
+        }
+        puts("");
+    }
+    return 0;
+}
