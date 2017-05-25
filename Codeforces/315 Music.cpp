a4f71144dde95daddee556fbbaa03faa30331eda
Little Von <122691411@qq.com>
2017-05-25 21:28:21 +0800

    315 A题
    //假定放到未下载点时候用时x秒，那么（q-1)/q就是每秒下载的速度，（q-1)/q*x+s=x。那么x=q*s
    //每次更新s，直到 s大于等于t

--- Codeforces/315 Music.cpp
+++ Codeforces/315 Music.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+//假定放到未下载点时候用时x秒，那么（q-1)/q就是每秒下载的速度，（q-1)/q*x+s=x。那么x=q*s
+//每次更新s，直到 s大于等于t
 int main(int argc, char const *argv[])
 {
     int t, s, q, cnt;
