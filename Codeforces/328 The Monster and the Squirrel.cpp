bdaf2656ae193de150542b8f0c8930ee94d08ae2
Little Von <122691411@qq.com>
2017-06-11 10:59:45 +0800

    328 B题
    找到规律即为答案

---
+++ Codeforces/328 The Monster and the Squirrel.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    long long n;
+    while (~scanf("%I64d", &n))
+    {
+        printf("%I64d\n", (n-2)*(n-2));
+    }
+    return 0;
+}
