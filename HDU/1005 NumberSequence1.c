8296a5fef656da03d8655e2299ea1c81e5840903
Little Von <122691411@qq.com>
2017-08-03 22:41:08 +0800

    change

--- HDU/1005 NumberSequence1.c
+++ HDU/1005 NumberSequence1.c
@@ -3,20 +3,20 @@ int main(int argc, char const *argv[])
 {
 	int arr[10000];
 	int a, b, n;
-	arr[1]=arr[2]=1;
-	while(scanf("%d %d %d",&a,&b,&n)!=EOF&&(a||b||n))
+	arr[1] = arr[2] = 1;
+	while (scanf("%d %d %d", &a, &b, &n) != EOF && (a || b || n))
 	{
-		int i;
-		for(i=3;i<10000;i++)
+		int T;
+		for (T = 3; T < 10000; T++)
 		{
-			arr[i]=(a*arr[i-1]+b*arr[i-2])%7;
-			if(arr[i]==1&&arr[i-1]==1) break;            
-			//如果有两个连着 =1，则后面的全部和前面相同，即出现了周期  
-            //这时就没必要再进行下去了，跳出循环, i-2为周期   
+			arr[T] = (a * arr[T - 1] + b * arr[T - 2]) % 7;
+			if (arr[T] == 1 && arr[T - 1] == 1) break;
+			//如果有两个连着 =1，则后面的全部和前面相同，即出现了周期
+			//这时就没必要再进行下去了，跳出循环, i-2为周期
 		}
-		n=n%(i-2);
-		arr[0]=arr[i-2];
-		// 把n对周期求模，当n = i-2时, n=0,此时本来应该取arr[i-2]的，所以把arr[0]=arr[i-2]  
+		n = n % (i - 2);
+		arr[0] = arr[i - 2];
+		// 把n对周期求模，当n = i-2时, n=0,此时本来应该取arr[i-2]的，所以把arr[0]=arr[i-2]
 		printf("%d\n", arr[n]);
 	}
 	return 0;
